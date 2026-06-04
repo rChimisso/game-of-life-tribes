937a12e2b184f43632b275b10cb229581eb116ce
Crystal Spider <riccardo.chimisso@gmail.com>
2026-06-04 14:30:14 +0200

    Improve wildfire preset.

--- src/app/feature/home/model/preset.ts
+++ src/app/feature/home/model/preset.ts
@@ -64,6 +64,13 @@ const WILDFIRE_MEDIUM_VEGETATION_TRIBE = 'Green';
 const WILDFIRE_DARK_VEGETATION_TRIBE = 'Dark Green';
 
 /**
+ * Ember tribe ID for the Wildfire preset.
+ *
+ * @type {string}
+ */
+const WILDFIRE_EMBER_TRIBE = 'Ember';
+
+/**
  * Fire tribe ID for the Wildfire preset.
  *
  * @type {string}
@@ -71,11 +78,25 @@ const WILDFIRE_DARK_VEGETATION_TRIBE = 'Dark Green';
 const WILDFIRE_FIRE_TRIBE = 'Fire';
 
 /**
+ * Blaze tribe ID for the Wildfire preset.
+ *
+ * @type {string}
+ */
+const WILDFIRE_BLAZE_TRIBE = 'Blaze';
+
+/**
  * Ash tribe ID for the Wildfire preset.
  *
  * @type {string}
  */
 const WILDFIRE_ASH_TRIBE = 'Ash';
+
+/**
+ * Rock tribe ID for the Wildfire preset.
+ *
+ * @type {string}
+ */
+const WILDFIRE_ROCK_TRIBE = 'Rock';
 
 /**
  * Named application preset.
@@ -99,7 +120,7 @@ export interface Preset {
   /**
    * Ruleset loaded by the preset.
    *
-   * @type {Ruleset}
+   * @type {Omit<Ruleset, 'cols' | 'rows'>}
    */
   readonly ruleset: Omit<Ruleset, 'cols' | 'rows'>;
 }
@@ -538,21 +559,88 @@ export const WILDFIRE_PRESET: Preset = {
         color: '0d5c2a'
       },
       {
+        id: WILDFIRE_EMBER_TRIBE,
+        color: 'ffae3d'
+      },
+      {
         id: WILDFIRE_FIRE_TRIBE,
-        color: 'ff5a1f'
+        color: 'fe7f2a'
+      },
+      {
+        id: WILDFIRE_BLAZE_TRIBE,
+        color: 'ff4d00'
       },
       {
         id: WILDFIRE_ASH_TRIBE,
         color: '6f6f6f'
+      },
+      {
+        id: WILDFIRE_ROCK_TRIBE,
+        color: '8a8f98'
       }
     ],
     rules: [
       {
         clause: {
           kind: IS_CLAUSE_KIND,
+          tribes: [WILDFIRE_BLAZE_TRIBE]
+        },
+        tribe: WILDFIRE_FIRE_TRIBE
+      },
+      {
+        clause: {
+          kind: IS_CLAUSE_KIND,
           tribes: [WILDFIRE_FIRE_TRIBE]
         },
+        tribe: WILDFIRE_EMBER_TRIBE
+      },
+      {
+        clause: {
+          kind: IS_CLAUSE_KIND,
+          tribes: [WILDFIRE_EMBER_TRIBE]
+        },
         tribe: WILDFIRE_ASH_TRIBE
+      },
+      {
+        clause: {
+          kind: IS_CLAUSE_KIND,
+          tribes: [WILDFIRE_ROCK_TRIBE]
+        },
+        tribe: WILDFIRE_ROCK_TRIBE
+      },
+      {
+        clause: {
+          kind: AND_CLAUSE_KIND,
+          clauses: [
+            {
+              kind: IS_CLAUSE_KIND,
+              tribes: [WILDFIRE_LIGHT_VEGETATION_TRIBE]
+            },
+            {
+              kind: MIN_CLAUSE_KIND,
+              value: 3,
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
+            }
+          ]
+        },
+        tribe: WILDFIRE_BLAZE_TRIBE
+      },
+      {
+        clause: {
+          kind: AND_CLAUSE_KIND,
+          clauses: [
+            {
+              kind: IS_CLAUSE_KIND,
+              tribes: [WILDFIRE_LIGHT_VEGETATION_TRIBE]
+            },
+            {
+              kind: MIN_CLAUSE_KIND,
+              value: 2,
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
+            }
+          ]
+        },
+        tribe: WILDFIRE_FIRE_TRIBE
       },
       {
         clause: {
@@ -565,7 +653,41 @@ export const WILDFIRE_PRESET: Preset = {
             {
               kind: MIN_CLAUSE_KIND,
               value: 1,
-              tribes: [WILDFIRE_FIRE_TRIBE]
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
+            }
+          ]
+        },
+        tribe: WILDFIRE_EMBER_TRIBE
+      },
+      {
+        clause: {
+          kind: AND_CLAUSE_KIND,
+          clauses: [
+            {
+              kind: IS_CLAUSE_KIND,
+              tribes: [WILDFIRE_MEDIUM_VEGETATION_TRIBE]
+            },
+            {
+              kind: MIN_CLAUSE_KIND,
+              value: 4,
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
+            }
+          ]
+        },
+        tribe: WILDFIRE_BLAZE_TRIBE
+      },
+      {
+        clause: {
+          kind: AND_CLAUSE_KIND,
+          clauses: [
+            {
+              kind: IS_CLAUSE_KIND,
+              tribes: [WILDFIRE_MEDIUM_VEGETATION_TRIBE]
+            },
+            {
+              kind: MIN_CLAUSE_KIND,
+              value: 3,
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
             }
           ]
         },
@@ -582,7 +704,41 @@ export const WILDFIRE_PRESET: Preset = {
             {
               kind: MIN_CLAUSE_KIND,
               value: 2,
-              tribes: [WILDFIRE_FIRE_TRIBE]
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
+            }
+          ]
+        },
+        tribe: WILDFIRE_EMBER_TRIBE
+      },
+      {
+        clause: {
+          kind: AND_CLAUSE_KIND,
+          clauses: [
+            {
+              kind: IS_CLAUSE_KIND,
+              tribes: [WILDFIRE_DARK_VEGETATION_TRIBE]
+            },
+            {
+              kind: MIN_CLAUSE_KIND,
+              value: 5,
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
+            }
+          ]
+        },
+        tribe: WILDFIRE_BLAZE_TRIBE
+      },
+      {
+        clause: {
+          kind: AND_CLAUSE_KIND,
+          clauses: [
+            {
+              kind: IS_CLAUSE_KIND,
+              tribes: [WILDFIRE_DARK_VEGETATION_TRIBE]
+            },
+            {
+              kind: MIN_CLAUSE_KIND,
+              value: 4,
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
             }
           ]
         },
@@ -599,11 +755,11 @@ export const WILDFIRE_PRESET: Preset = {
             {
               kind: MIN_CLAUSE_KIND,
               value: 3,
-              tribes: [WILDFIRE_FIRE_TRIBE]
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
             }
           ]
         },
-        tribe: WILDFIRE_FIRE_TRIBE
+        tribe: WILDFIRE_EMBER_TRIBE
       },
       {
         clause: {
@@ -624,7 +780,7 @@ export const WILDFIRE_PRESET: Preset = {
             },
             {
               kind: NONE_CLAUSE_KIND,
-              tribes: [WILDFIRE_FIRE_TRIBE]
+              tribes: [WILDFIRE_EMBER_TRIBE, WILDFIRE_FIRE_TRIBE, WILDFIRE_BLAZE_TRIBE]
             },
             {
               kind: MIN_CLAUSE_KIND,
